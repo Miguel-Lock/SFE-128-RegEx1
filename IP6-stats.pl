9b8ce966e2a04a9c9f7741851869df104411374a
Miguel-Lock <miguellock333@gmail.com>
2023-04-10 21:14:21 -0400

    Printed shellell print statemtns

--- IP6-stats.pl
+++ IP6-stats.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
+use v5.10;
 
-print("This a test\n");
 
+sub main {
+	$hold = "#";
+
+	say "IP6 Connections: " . $hold . "\n";
+
+	say "Machine count: " . $hold . "\n";
+
+	say "Connections: " . $hold;
+	say "Connections cont.: one line per machine. <tab><count><tab><Unique Mac Address>";
+}
+
+
+
+
+
+main();
 
