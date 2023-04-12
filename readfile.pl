92d56ec7a9d9eeabb218655291cf2050ef54b05c
Miguel-Lock <miguellock333@gmail.com>
2023-04-12 17:40:47 -0400

    Making progres on input file lines

--- readfile.pl
+++ readfile.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
 use v3.10;
 
-open CONFIG, 'logs';
+my $filename = 'testtext.txt';
+open(FILE, "<", $filename) or die "Can't open $filename"; #opens file
+my @lines = <FILE>; #reads each line from FILE into array
+close(FILE); #closes FILE
 
 
+for(@lines) {
+	print $_; #prints line one by one
+}
 
-print($logs);
+
+print $#lines # note that this is index, meaning result + 1 = total array values
